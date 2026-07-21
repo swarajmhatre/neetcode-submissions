18a82514b8797e9151eca12316c8e9649435f3a7
Swaraj Mhatre <83057854+swarajmhatre@users.noreply.github.com>
2026-07-22 00:05:11 +0530

    Add: merge-two-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-2.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+   public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* newnode = nullptr;
+        ListNode* resultnode = nullptr;
+        ListNode* head = nullptr;
+        if (list1 == nullptr) return list2;
+        if (list2 == nullptr) return list1;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val <= list2->val) {
+                if (head == nullptr) {
+                    head = new ListNode(list1->val);
+                    resultnode = head;
+                } else {
+                    resultnode->next = new ListNode(list1->val);
+                    resultnode = resultnode->next;
+                }
+                list1 = list1->next;
+            } else if (list1->val >= list2->val) {
+                if (head == nullptr) {
+                    head = new ListNode(list2->val);
+                    resultnode = head;
+                } else {
+                    resultnode->next = new ListNode(list2->val);
+                    resultnode = resultnode->next;
+                }
+                list2 = list2->next;
+            }
+        }
+
+        if (list1 != nullptr) {
+            while (list1 != nullptr) {
+                resultnode->next = new ListNode(list1->val);
+                resultnode = resultnode->next;
+                list1 = list1->next;
+            }
+        } else if (list2 != nullptr) {
+            while (list2 != nullptr) {
+                resultnode->next = new ListNode(list2->val);
+                resultnode = resultnode->next;
+                list2 = list2->next;
+            }
+        }
+        return head;
+    }
+};
