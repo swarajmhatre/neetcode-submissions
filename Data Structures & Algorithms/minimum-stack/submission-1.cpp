e91018eb77e77fd3565e0a596e8150285226a439
Swaraj Mhatre <83057854+swarajmhatre@users.noreply.github.com>
2026-07-23 23:36:25 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,48 @@
+class MinStack {
+    struct Node {
+        int iVal;
+        Node* next;
+        int iMinSoFar;
+        Node(int iNo) : iVal(iNo), next(nullptr) {}
+        Node(int iNo, Node* next) : iVal(iNo), next(next) {}
+    };
+
+    Node* head;
+
+   public:
+    MinStack() { head = nullptr; }
+
+    void push(int val) {
+        if (head) {
+            Node* newNode = new Node(val, head);
+            (val < head->iMinSoFar) ? newNode->iMinSoFar = val : newNode->iMinSoFar = head->iMinSoFar ;
+            head = newNode;
+        } else {
+            head = new Node(val);
+            head->iMinSoFar = val;
+        }
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack Underflow: Cannot view top of empty stack.");
+        }
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    int top() {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack Underflow: Cannot view top of empty stack.");
+        }
+        return head->iVal;
+    }
+
+    int getMin() {
+        
+        return head->iMinSoFar;
+    }
+
+    bool isEmpty() { return head == nullptr; }
+};
