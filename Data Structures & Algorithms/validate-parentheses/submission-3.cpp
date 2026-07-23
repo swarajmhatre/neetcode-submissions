8ecefbff1b7d32689267ce2329e1c9edfa6eb220
Swaraj Mhatre <83057854+swarajmhatre@users.noreply.github.com>
2026-07-23 23:51:52 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> container;
+        char a = '{', b = '[', c = '(', p = '}', q = ']', r = ')';
+
+        if(s.length()%2 == 1) return false;
+        for(int i =0; i < s.length(); i++){
+            if(s[i] == a || s[i] == b || s[i] == c){
+                container.push(s[i]);
+            }
+            else if(s[i] == p || s[i] == q || s[i] == r){
+                if(container.empty()) return false;
+                if(s[i] == p && container.top() == a) container.pop();
+                else if(s[i] == q && container.top() == b) container.pop();
+                else if(s[i] == r && container.top() == c) container.pop();
+                else return false;
+            }
+
+        }
+        if(container.empty()) return true;
+        return false;
+    }
+};
