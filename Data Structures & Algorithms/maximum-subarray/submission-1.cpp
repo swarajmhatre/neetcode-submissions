369fb75992bc13d05ee52d97b6dc6bc91762589b
Swaraj Mhatre <83057854+swarajmhatre@users.noreply.github.com>
2026-07-24 22:55:56 +0530

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+//Kadane's Algo
+    int maxSubArray(vector<int>& nums) {
+        int res = nums[0];
+        int maxending = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            maxending = max(maxending + nums[i], nums[i]);
+
+            res = max(res, maxending);
+        }
+        return res;
+    }
+};
