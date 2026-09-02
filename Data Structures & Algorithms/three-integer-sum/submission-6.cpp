cf2306aba95ba46558b791e409eced6d9d4926ee
Swaraj Mhatre <83057854+swarajmhatre@users.noreply.github.com>
2026-09-02 23:45:28 +0530

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.cpp
@@ -0,0 +1,29 @@
+class Solution {
+   public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        set<vector<int>> uniqueTrip;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++) {
+            int j = i + 1, k = n - 1;
+            if(nums[i] > 0) break;
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    vector<int> Trip = {nums[i], nums[j], nums[k]};
+                    if (uniqueTrip.find(Trip) == uniqueTrip.end()) {
+                        uniqueTrip.insert(Trip);
+                    }
+                    j++;
+                    k--;
+                } else if (nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        vector<vector<int>> ans(uniqueTrip.begin(), uniqueTrip.end());
+
+        return ans;
+    }
+};
